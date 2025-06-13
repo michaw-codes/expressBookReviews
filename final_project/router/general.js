41accfc1be62296c2b6d230329b0cf12e51c7c3e
michaw-codes <111310362+michaw-codes@users.noreply.github.com>
2025-06-13 18:07:51 -0400

    feat(): implement authentication and 2 endpoints

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,14 +12,16 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    return res.status(300).json({message: books});
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = parseInt(req.params.isbn);
+    if (isbn in books) {
+        return res.status(200).json({message: books[isbn]});
+    }
+    return res.status(404).json({message: "book not found"});
  });
   
 // Get book details based on author
